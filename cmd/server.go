1e48173d551a30eaea658f4c6a383463d05a1f66
Eric Liao <rcliao01@gmail.com>
2017-08-24 23:04:54 -0700

    feat: add basic web interface to test

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -14,8 +15,11 @@ test sql without installing CLI or its dependencies (e.g. MySQL)
 
 func main() {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/hello", web.Hello()).Methods("GET")
 	r.HandleFunc("/", web.Index()).Methods("GET")
+	r.HandleFunc("/api/test", web.RunTest()).Methods("POST")
 
+	fmt.Println("Running web server at port 8000")
 	http.ListenAndServe(":8000", r)
 }
--- web/handlers.go
+++ web/handlers.go
@@ -1,9 +1,18 @@
 package web
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	tester "github.com/rcliao/sql-unit-test"
+	"github.com/rcliao/sql-unit-test/parser"
+	"github.com/rcliao/sql-unit-test/runner"
 )
 
 // Hello says hello
@@ -22,4 +31,79 @@ func Index() http.HandlerFunc {
 		}
 		t.Execute(w, nil)
 	})
+}
+
+// RunTest handles the query from the request param to test them
+func RunTest() http.HandlerFunc {
+	testCasesContent, err := ioutil.ReadFile("./testcase.json")
+	if err != nil {
+		panic(err)
+	}
+	setupContent, err := ioutil.ReadFile("./setup.sql")
+	if err != nil {
+		panic(err)
+	}
+	teardownContent, err := ioutil.ReadFile("./teardown.sql")
+	if err != nil {
+		panic(err)
+	}
+	testCases, err := parser.ParseTestCases(string(testCasesContent))
+	if err != nil {
+		panic(err)
+	}
+	var setupStatements = []tester.Statement{}
+	if string(setupContent) != "" {
+		setupStatements = parser.ParseSQL(string(setupContent), "#")
+		if err != nil {
+			panic(err)
+		}
+	}
+	var teardownStatements = []tester.Statement{}
+	if string(teardownContent) != "" {
+		teardownStatements = parser.ParseSQL(string(teardownContent), "#")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		db := getDB()
+		runner := runner.NewMySQLRunner(db)
+
+		submission := r.FormValue("statements")
+		statements := parser.ParseSQL(string(submission), "#")
+		pass, err := tester.Run(runner, statements, setupStatements, teardownStatements, testCases)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var result = "All test passes!"
+		if !pass {
+			result = "There are tests failing"
+		}
+
+		fmt.Fprintln(w, result)
+	})
+}
+
+func getDB() *sql.DB {
+	// TODO: generate a random DB string
+	defaultProtocol := "tcp"
+	defaultPort := "3306"
+	sqlDSN := fmt.Sprintf(
+		"%s:%s@%s(%s:%s)/%s",
+		os.Getenv("MYSQL_USERNAME"),
+		os.Getenv("MYSQL_PASSWORD"),
+		defaultProtocol,
+		os.Getenv("MYSQL_HOST"),
+		defaultPort,
+		os.Getenv("MYSQL_DB"),
+	)
+
+	db, err := sql.Open("mysql", sqlDSN)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
