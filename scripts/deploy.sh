8b01f9c7feb3d033194eed63e77c768cfe78f6c8
Eric Liao <rcliao01@gmail.com>
2017-09-02 19:14:58 -0700

    feat: add deploy and restart script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./scripts/build-web.sh && rsync -av . deploy@ta.do:/opt/sql-unit-test
+./scripts/build-web.sh && rsync -av . deploy@ta.do:/opt/sql-unit-test && ssh 'deploy@ta.do' 'supervisorctl restart sql-unit-test'
 
